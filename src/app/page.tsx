89543522978a2e5e53211b6dc43681956aa497e0
unknown <zulfikrimirza@yahoo.com>
2024-04-26 14:30:33 +0700

    database

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import { db } from "@vercel/postgres";
 import Link from "next/link";
 
 const mockUrls = [
@@ -12,12 +13,20 @@ const mockImages = mockUrls.map((url, index) => ({
   id: index + 1,
   url,
 }))
-export default function HomePage() {
+
+export default async function HomePage() {
+  const posts = await db.query.posts.findMany();
+
+  console.log(posts);
+
   return (
     <main className= "">
-      <div className="flex flex-wrap gap-4">{
-        [...mockImages, ...mockImages, ...mockImages].map((image) => (
-          <div key={image.id} className="w-48">
+      <div className="flex flex-wrap gap-4">
+        {posts.map((post) => (
+        <div key={post.id}>{post.name}</div>
+      ))}
+        {[...mockImages, ...mockImages, ...mockImages].map((image, index) => (
+          <div key={image.id + "-" + index} className="w-48">
             <img src={image.url}/>
           </div>
         ))
